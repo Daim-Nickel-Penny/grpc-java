07f67d5e78a8cb08cc20782a14f3a0890d73f0ed
Daim-Nickel-Penny <1804449@kiit.ac.in>
2022-12-03 16:00:20 +0530

    Refactoring GreetClient.

--- src/main/java/com/greetGrpc/greet/client/GreetClient.java
+++ src/main/java/com/greetGrpc/greet/client/GreetClient.java
@@ -1,45 +1,67 @@
 package com.greetGrpc.greet.client;
+
 import com.proto.greet.*;
 import com.proto.uno.UnoServiceGrpc;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 
 public class GreetClient {
+
     public static void main(String[] args) {
         System.out.println("Client Start");
 
+        GreetClient main = new GreetClient();
+        main.run();
+    }
+
+    private void run() {
+        System.out.println("Creating Stub");
         ManagedChannel channel = ManagedChannelBuilder.
                 forAddress("localhost", 50051).
                 usePlaintext().
                 build();
-        System.out.println("Creating Stub");
 
-        // old code
-        //UnoServiceGrpc.UnoServiceBlockingStub syncClient = UnoServiceGrpc.newBlockingStub(channel);
 
+        doUnaryCall(channel);
+        doServerStreamingCall(channel);
+
+
+        System.out.println("Shutting Channel");
+        channel.shutdown();
+
+    }
+
+    public void doUnaryCall(ManagedChannel channel) {
         // created  a greet service client (blocking = synchronous )
         GreetServiceGrpc.GreetServiceBlockingStub greetClient =
                 GreetServiceGrpc.newBlockingStub(channel);
 
-         /*
         //Unary
+
         // created a protocol buffer greeting message
         Greeting greeting = Greeting.newBuilder()
                 .setFirstName("Foo")
                 .setLastName("Bar")
                 .build();
+
         // do same for Greet request
         GreetRequest greetRequest = GreetRequest.newBuilder()
                 .setGreeting(greeting)
                 .build();
+
         // call the RPC and get back a GreetResponse (protocol buffers )
         // this greet is coming from greetServer
         GreetResponse greetResponse = greetClient.greet(greetRequest);
         System.out.println(greetResponse.getResult());
-        */
+    }
 
+    public void doServerStreamingCall(ManagedChannel channel) {
+        // created  a greet service client (blocking = synchronous )
+        GreetServiceGrpc.GreetServiceBlockingStub greetClient =
+                GreetServiceGrpc.newBlockingStub(channel);
 
         // Server Streaming
+
         // prepare request
         GreetManyTimesRequest greetManyTimesRequest =
                 GreetManyTimesRequest.newBuilder()
@@ -51,9 +73,6 @@ public class GreetClient {
                 .forEachRemaining(greetManyTimesResponse -> {
                     System.out.println(greetManyTimesResponse.getResult());
                 });
-
-        System.out.println("Shutting Channel");
-        channel.shutdown();
-
     }
+
 }
--- src/main/java/com/greetGrpc/greet/server/GreetServiceImpl.java
+++ src/main/java/com/greetGrpc/greet/server/GreetServiceImpl.java
@@ -54,4 +54,44 @@ public class GreetServiceImpl extends GreetServiceGrpc.GreetServiceImplBase {
 
         }
     }
+
+    @Override
+    public StreamObserver<LongGreetRequest> longGreet(StreamObserver<LongGreetResponse> responseObserver) {
+
+        StreamObserver<LongGreetRequest> requestObserver = new StreamObserver<LongGreetRequest>() {
+            String result = "";
+
+            @Override
+            public void onNext(LongGreetRequest value) {
+                //client sends a message
+
+                result += " ,Hello " + value.getGreeting().getFirstName() + " ! ";
+
+
+            }
+
+            @Override
+            public void onError(Throwable t) {
+                //client sends an error
+            }
+
+            @Override
+            public void onCompleted() {
+                //client is done
+
+
+                // this is when we want to return a response (responseObserver)
+
+                responseObserver.onNext(
+                        LongGreetResponse.newBuilder()
+                                .setResult(result)
+                                .build()
+                );
+
+                responseObserver.onCompleted();
+            }
+
+        };
+        return requestObserver;
+    }
 }
